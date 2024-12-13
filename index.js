4be01039a92e152a8065b20b1dcd11a42dd021a5
Amiz <lgmalshan@gmail.com>
2024-12-14 01:26:02 +0530

    Fixed errors 01

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-const moduleName = document.getElementById("name").textContent;
-const moduleCredit = document.getElementById("credit").value;
-const moduleGrade = document.querySelector("select").value;
+const moduleName = document.getElementById("name");
+const moduleCredit = document.getElementById("credit");
+const moduleGrade = document.querySelector("select");
 const modules = document.getElementById("modules");
 const gpaDisplay = document.getElementById("gpaDisplay");
 
-let gpa = 0.00;
 let moduleCount = 0;
 let value = 0.0;
-//gpa = parseFloat(window.prompt("Enter current GPA: "));
+let gpa = parseFloat(window.prompt("Enter current GPA:", "0.00")) || 0.0;
+
+value = Number(gpa);
 gpaDisplay.textContent = gpa.toFixed(2);
 
-function calcGPA(){
-    moduleCount++;
-    value =+ moduleCredit * moduleGrade;
-    gpa /= moduleCount;
+function calcGPA(event){
+    if (event) {
+        event.preventDefault();
+    }
 
+    const credit = Number(moduleCredit.value);
+    const grade = Number(moduleGrade.value);
+    const name = moduleName.value;
+
+    moduleCount += credit;
+    value += credit * grade;
+    gpa = value / moduleCount;
+    
     gpaDisplay.textContent = gpa.toFixed(2);
 
     const moduleDisplay = document.createElement("p");
-    moduleDisplay.textContent = `${moduleName} ${moduleCredit} ${moduleGrade}`;
+    moduleDisplay.textContent = `Module:${name} | Credit:${grade} | Grade:${credit}`;
     modules.appendChild(moduleDisplay);
 }
+
